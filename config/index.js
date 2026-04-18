4bfd3d7bc62bf0a293b845b515cbe0de0f9062ef
tushar981146 <tusharblackhathacker@gmail.com>
2026-04-18 22:33:25 +0530

    cors

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ const config = {
     },
 
     cors: {
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
-        Credential: true,
+        origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+        credentials: true,
         methods: ["get"]
     }
 }
